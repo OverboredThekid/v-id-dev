f396c6e1271c5f114bae40fae2e7bd2179163cfe
Bennett haynes <bennetthaynes1@icloud.com>
2022-12-22 16:38:00 -0600

    first commit

--- app/Http/Livewire/Svg.php
+++ app/Http/Livewire/Svg.php
@@ -8,13 +8,12 @@ use App\Models\staff_prints;
 
 class Svg extends Component
 {
-    public $staff, $staff_img;
+    public $staff;
  
     public function mount($id)
     {
-        $media = staff_prints::findOrFail($id);
-        $this->staff = $media;
-        $this->staff_img = $media->getMedia('staff_print');
+        $staff_info = staff_prints::findOrFail($id);
+        $this->staff = $staff_info;
     }
 
 
