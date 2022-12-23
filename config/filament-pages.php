873f1a70cc895ec36bd28849862425e7c75fdcb8
Bennett haynes <bennetthaynes1@icloud.com>
2022-12-23 00:08:20 -0600

    first commit

--- config/filament-pages.php
+++ config/filament-pages.php
@@ -4,7 +4,7 @@ use Beier\FilamentPages\Filament\FilamentPageTemplates\DefaultTemplate;
 use Beier\FilamentPages\Filament\Resources\FilamentPageResource;
 use Beier\FilamentPages\Models\FilamentPage;
 use Beier\FilamentPages\Renderer\SimplePageRenderer;
-use App\Filament\Resources\FilamentPageResource\extra;
+use App\Filament\Resources\Page\extra;
 
 return [
     'filament' => [
