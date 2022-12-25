c773c2d2ccb42bf4388816a5554ac3f68a6f8582
Bennett haynes <bennetthaynes1@icloud.com>
2022-12-24 19:47:28 -0600

    first commit

--- config/filament-blog.php
+++ config/filament-blog.php
@@ -7,10 +7,24 @@ return [
      *      \Filament\Forms\Components\RichEditor::class
      *      \Filament\Forms\Components\MarkdownEditor::class
      */
-    'editor'  => FilamentEditorJs\Forms\Components\EditorJs::class,
+    'editor'  => \Filament\Forms\Components\RichEditor::class,
 
     /**
      * Buttons for text editor toolbar.
      */
-
+    'toolbar_buttons' => [
+        'attachFiles',
+        'blockquote',
+        'bold',
+        'bulletList',
+        'codeBlock',
+        'h2',
+        'h3',
+        'italic',
+        'link',
+        'orderedList',
+        'redo',
+        'strike',
+        'undo',
+    ],
 ];
