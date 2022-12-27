f1dd2ac55aa2c56b422cd6166d35f14041eaeb1c
Bennett haynes <bennetthaynes1@icloud.com>
2022-12-27 13:37:54 -0600

    first commit

--- resources/views/livewire/linkTree.blade.php
+++ resources/views/livewire/linkTree.blade.php
@@ -1,32 +1,3 @@
 <div>
-<div class="container mx-auto">
-        <div class="col-xs-12">
-            <div class="text-center py-10">
-                <img class="backdrop linktree">
-                <h2 class="text-white py-5">{{ config('app.name', 'Company Links') }}</h2>
-            </div>
-        </div>
-    </div>
-    <div class="container mx-auto">
-        <div class="col-xs-12">
-            <div class="text-center">
-                <div class="py-5">
-                    <button onclick="location.href='#'" class="text-white-500 border border-red-500 hover:bg-red-500 hover:text-white active:bg-red-600 font-bold uppercase w-1/2 px-8 py-3 rounded-full outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Staff Links</button>
-                </div>
-                <div class="py-5">
-                    <button onclick="location.href='#'" class="text-white-500 border border-red-500 hover:bg-red-500 hover:text-white active:bg-red-600 font-bold uppercase w-1/2 px-8 py-3 rounded-full outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Staff Links</button>
-                </div>
-                <div class="py-5">
-                    <button onclick="location.href='#'" class="text-white-500 border border-red-500 hover:bg-red-500 hover:text-white active:bg-red-600 font-bold uppercase w-1/2 px-8 py-3 rounded-full outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Staff Links</button>
-                </div>
-                <div class="py-5">
-                    <button onclick="location.href='#'" class="text-white-500 border border-red-500 hover:bg-red-500 hover:text-white active:bg-red-600 font-bold uppercase w-1/2 px-8 py-3 rounded-full outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Staff Links</button>
-                </div>
-                <div class="py-5">
-                    <button onclick="location.href='#'" class="text-white-500 border border-red-500 hover:bg-red-500 hover:text-white active:bg-red-600 font-bold uppercase w-1/2 px-8 py-3 rounded-full outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">Staff Links</button>
-                </div>
-            </div>
-        </div>
-    </div>
 {!! $slug->data['content']['content'] !!}
 </div>
