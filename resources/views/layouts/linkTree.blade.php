5d6b2ebe583baeb0b899d2a58c1263242c1dbd8c
Bennett haynes <bennetthaynes1@icloud.com>
2022-12-27 13:32:50 -0600

    first commit

--- resources/views/layouts/linkTree.blade.php
+++ resources/views/layouts/linkTree.blade.php
@@ -9,20 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.css" rel="stylesheet">
 </head>
-<style>
-    body {
-        width: 100wh;
-        height: 90vh;
-        color: #fff;
-        background: linear-gradient(-45deg, #EE7752, #E73C7E, #23A6D5, #23D5AB);
-        background-size: 400% 400%;
-        -webkit-animation: Gradient 15s ease infinite;
-        -moz-animation: Gradient 15s ease infinite;
-        animation: Gradient 15s ease infinite;
-    }
-</style>
-
-<body>
+<body class="bg-gradient-to-r from-rose-400 to-red-500">
     {{ $slot }}
 </body>
 <footer>
