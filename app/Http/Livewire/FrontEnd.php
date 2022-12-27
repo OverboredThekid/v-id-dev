449624a5e648d98ddcdb48169fc9a13f840d2063
Bennett haynes <bennetthaynes1@icloud.com>
2022-12-27 11:37:58 -0600

    first commit

--- app/Http/Livewire/FrontEnd.php
+++ app/Http/Livewire/FrontEnd.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use Beier\FilamentPages\Models\FilamentPage as DB;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class FrontEnd extends Component
 {
@@ -12,7 +13,15 @@ class FrontEnd extends Component
 
     public function mount($slug)
     {
-        $this->page = DB::findorfail($slug);
+        // Will return a ModelNotFoundException if no user with that id
+        try {
+            $this->page = DB::findorfail($slug);
+        }
+        // catch(Exception $e) catch any exception
+        catch (ModelNotFoundException $e) {
+            dd(get_class_methods($e)); // lists all available methods for exception object
+            dd($e);
+        }
     }
 
     public function render()
