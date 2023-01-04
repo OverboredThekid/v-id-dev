d4f982804d5fb69677114e94c400332ca1b1617d
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-04 16:33:57 -0600

    first commit

--- app/Http/Livewire/PhotoSection.php
+++ app/Http/Livewire/PhotoSection.php
@@ -4,72 +4,28 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use Livewire\WithFileUploads;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class PhotoSection extends Component
 {
     use WithFileUploads;
-    public $photo = null;
-    public $showWebcam = false;
 
-    public function openFileInput()
-    {
-        $this->photo = null;
-        $this->emit('openFileInput');
-    }
-
-    public function upload($event)
-    {
-        $this->photo = $event->target->files[0];
-        $this->emit('photoSelected');
-    }
-
-    public function capture()
-    {
-        $video = $this->get('video');
-        $image = $this->get('image');
-        $canvas = $this->get('canvas');
-
-        $image->src = $canvas->toDataURL('image/png');
-        $canvas->getContext('2d')->drawImage($video, 0, 0, $canvas->width, $canvas->height);
-        $this->photo = $image->src;
-        $this->emit('photoSelected');
-    }
-
-    public function initCropper()
-    {
-        $cropper = $this->get('cropper');
-        $image = $this->get('image');
-
-        $cropper->cropper([
-            'aspectRatio' => 1,
-            'viewMode' => 2,
-            'preview' => '.img-preview',
-        ]);
-        
-
-        $image->src = $this->photo;
-    }
-
-    public function crop()
-    {
-        $cropper = $this->get('cropper');
-        $image = $this->get('image');
-
-        $croppedData = $cropper->cropper('getData');
-
-        $canvas = $this->get('canvas');
-        $canvas->getContext('2d')->drawImage($image, $croppedData['x'], $croppedData['y'], $croppedData['width'], $croppedData['height'], 0, 0, $canvas->width, $canvas->height);
-
-        $this->photo = $canvas->toDataURL('image/png');
-        $this->emit('photoCropped');
-    }
+    public $photo;
+    public $croppedPhoto;
 
     public function render()
     {
-        return view('livewire.photo-section', [
-            'photo' => $this->photo,
-            'showWebcam' => $this->showWebcam,
-        ])->layout('layouts.photo');
+        return view('livewire.photo-section.photo-section');
+    }
 
+    public function cropPhoto($formData)
+    {
+        $croppedPhoto = Image::make($formData['cropped_photo']);
+        $this->croppedPhoto = $croppedPhoto->encode('data-url');
+    }
+
+    public function submitPhoto()
+    {
+        // Handle submission of final photo here...
     }
 }
--- resources/views/livewire/photo-section.blade.php
+++ resources/views/livewire/photo-section.blade.php
@@ -1,38 +1,88 @@
 <div>
-    <div wire:ignore.self class="container mt-5">
-        <div class="row">
-            <div class="col-md-9">
-                <!-- Webcam capture -->
-                <div x-data="{ showWebcam: false }" class="mb-3">
-                    <button class="btn btn-primary" @click="showWebcam = true">
-                        Capture Photo
-                    </button>
-                    <div x-show="showWebcam" class="webcam-container mt-3">
-                        <video x-ref="video" class="webcam"></video>
-                        <button class="btn btn-primary mt-3" @click="capture()">Take Photo</button>
-                    </div>
-                </div>
-
-                <!-- File upload -->
-                <div class="mb-3">
-                    <button class="btn btn-primary" wire:click="openFileInput">
-                        Select Photo
-                    </button>
-                    <input wire:model="photo" type="file" wire:change="upload" class="hidden" />
-                </div>
-
-                <!-- Cropped image preview -->
-                <div x-data="{ showCropper: false }" x-show="photo || showWebcam">
-                    <img x-ref="image" class="mb-3" />
-                    <button class="btn btn-primary" @click="showCropper = true">
-                        Crop Photo
-                    </button>
-                    <div x-show="showCropper" x-init="initCropper()" class="cropper-container mt-3">
-                        <img x-ref="cropper" class="cropper" />
-                        <button class="btn btn-primary mt-3" @click="crop()">Crop</button>
-                    </div>
-                </div>
+<div class="photo-section">
+    <div class="webcam-container" x-data="{ open: false }">
+        <button x-on:click="open = true">
+            Take a Photo
+        </button>
+        <div x-show="open" x-transition:enter="ease-out duration-200" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-150" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
+            <div class="overlay" x-on:click.away="open = false"></div>
+            <div class="modal" x-on:click.away="open = false">
+                <div id="webcam"></div>
+                <button class="btn btn-primary" x-on:click="open = false">
+                    Take Photo
+                </button>
             </div>
         </div>
     </div>
+
+    <div class="upload-container" x-data="{ open: false }">
+        <button x-on:click="open = true">
+            Upload a Photo
+        </button>
+        <input type="file" x-show="open" x-on:change="open = false" wire:model="photo" />
+    </div>
+
+    @if($photo)
+        <div class="cropper-container">
+            <img id="photo" src="{{ $photo->temporaryUrl() }}" />
+        </div>
+        <button class="btn btn-primary" wire:click="cropPhoto">
+            Crop Photo
+        </button>
+    @endif
+
+    @if($croppedPhoto)
+        <img src="{{ $croppedPhoto }}" />
+        <button class="btn btn-primary" wire:click="submitPhoto">
+            Submit Photo
+        </button>
+    @endif
+</div>
+@push('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.x.x/dist/alpine.min.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/cropperjs@1.x.x/dist/cropper.min.js" defer></script>
+    <script>
+        function startWebcam() {
+            Webcam.set({
+                width: 320,
+                height: 240,
+                image_format: 'jpeg',
+                jpeg_quality: 90
+            });
+            Webcam.attach('#webcam');
+        }
+
+        function stopWebcam() {
+            Webcam.reset();
+        }
+
+        function takeSnapshot() {
+            Webcam.snap(function(dataUri) {
+                document.getElementById('photo').src = dataUri;
+            });
+        }
+
+        function cropPhoto() {
+            var image = document.getElementById('photo');
+            var cropper = new Cropper(image, {
+                aspectRatio: 1,
+                crop: function(event) {
+                    console.log(event.detail.x);
+                    console.log(event.detail.y);
+                    console.log(event.detail.width);
+                    console.log(event.detail.height);
+                    console.log(event.detail.rotate);
+                    console.log(event.detail.scaleX);
+                    console.log(event.detail.scaleY);
+                }
+            });
+
+            cropper.getCroppedCanvas().toBlob((blob) => {
+                const formData = new FormData();
+                formData.append('cropped_photo', blob);
+                @this.call('cropPhoto', formData);
+            });
+        }
+    </script>
+@endpush
 </div>
