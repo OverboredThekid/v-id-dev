125b1e3e07368353f109feb28d1ebca8f65c9669
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-04 15:58:08 -0600

    first commit

--- resources/views/layouts/photo.blade.php
+++ resources/views/layouts/photo.blade.php
@@ -1,10 +1,8 @@
 <head>
     <title>{{ config('app.name') }} ~ photo</title>
-<!-- Include alpine.js -->
-<script src="{{ asset('vendor/alpine.js') }}"></script>
-<!-- Include cropper.js -->
-<link rel="stylesheet" href="{{ asset('vendor/cropper.js/cropper.css') }}">
-<script src="{{ asset('vendor/cropper.js/cropper.js') }}"></script> 
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.6/cropper.js"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js']) 
     @livewireStyles
 </head>
 <body>
