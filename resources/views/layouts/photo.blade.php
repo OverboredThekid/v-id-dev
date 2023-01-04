04b4a270071841d841dd831ef081730a664caff1
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-04 16:00:36 -0600

    first commit

--- resources/views/layouts/photo.blade.php
+++ resources/views/layouts/photo.blade.php
@@ -2,7 +2,6 @@
     <title>{{ config('app.name') }} ~ photo</title>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.6/cropper.js"></script>
-    @vite(['resources/css/app.css', 'resources/js/app.js']) 
     @livewireStyles
 </head>
 <body>
