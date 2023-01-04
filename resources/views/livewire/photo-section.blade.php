4aa9a8e500b63d0c92d712c9879f2d9ff6916723
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-04 13:59:13 -0600

    first commit

--- resources/views/livewire/photo-section.blade.php
+++ resources/views/livewire/photo-section.blade.php
@@ -1,26 +1,37 @@
 <div>
 <div x-data="{ photo: null }">
-  <video x-ref="video" x-init="$refs.video.muted = true; $refs.video.playsinline = true;" autoplay width="100%"></video>
-  <canvas x-ref="canvas" width="480" height="360"></canvas>
+  <video x-data x-ref="video" x-init="$refs.video.muted = true; $refs.video.playsinline = true;" autoplay width="100%"></video>
+  <canvas x-data x-ref="canvas" width="480" height="360"></canvas>
   <button x-on:click="capture()">Capture</button>
   <input type="file" x-ref="fileInput" accept="image/*" class="hidden">
   <button x-on:click="$refs.fileInput.click()">Choose File</button>
   <button x-bind:class="{ 'hidden': !photo }" x-on:click="photo = null">Clear</button>
   <div x-bind:class="{ 'hidden': !photo }">
     <div x-data="{ crop: { aspectRatio: 1 } }">
-      <img x-bind:src="photo" x-ref="photo" x-init="new Cropper($refs.photo, $data)">
+      <img x-data x-bind:src="photo" x-ref="photo" x-init="new Cropper($refs.photo, $data)">
       <button x-on:click="crop()">Crop</button>
     </div>
   </div>
 </div>
 
+<script src="/path/to/alpine.js"></script>
+<script src="/path/to/cropper.js"></script>
 <script>
   function capture() {
-    const video = this.$refs.video;
-    const canvas = this.$refs.canvas;
-    const context = canvas.getContext('2d');
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    this.photo = canvas.toDataURL();
+    const fileInput = this.$refs.fileInput;
+    if (fileInput.files && fileInput.files[0]) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.photo = e.target.result;
+      };
+      reader.readAsDataURL(fileInput.files[0]);
+    } else {
+      const video = this.$refs.video;
+      const canvas = this.$refs.canvas;
+      const context = canvas.getContext('2d');
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      this.photo = canvas.toDataURL();
+    }
   }
 
   function crop() {
@@ -29,4 +40,5 @@
     this.photo = canvas.toDataURL();
   }
 </script>
+
 </div>
