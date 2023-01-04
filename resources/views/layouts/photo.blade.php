51f49114836a55d33b81fbb9dd96a396a7102980
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-04 13:23:04 -0600

    first commit

--- resources/views/layouts/photo.blade.php
+++ resources/views/layouts/photo.blade.php
@@ -1,5 +1,7 @@
 <head>
     <title>{{ config('app.name') }} ~ photo</title>
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+
     @livewireStyles
 </head>
 <body>
