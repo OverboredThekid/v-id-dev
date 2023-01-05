f093a44928164d8f35baf573a031d9b52e6ee5e2
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-04 21:52:31 -0600

    first commit

--- app/Http/Livewire/PhotoSection.php
+++ app/Http/Livewire/PhotoSection.php
@@ -25,7 +25,7 @@ class PhotoSection extends Component
 
     public function render()
     {
-        return view('livewire.photo-section');
+        return view('livewire.photo-section')->layout('layouts.photo');
     }
 
     public function capturePhoto($photo)
