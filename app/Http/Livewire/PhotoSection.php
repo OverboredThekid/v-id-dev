0a981b114bdfc633a5ddc877a2f378db295b7812
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-05 16:24:19 -0600

    first commit

--- app/Http/Livewire/PhotoSection.php
+++ app/Http/Livewire/PhotoSection.php
@@ -13,137 +13,48 @@ class PhotoSection extends Component
 
 
     // State variables
-    public $captureModal = false;
-    public $uploadModal = false;
-    public $cropModal = false;
     public $photo;
-    public $croppedPhoto;
 
-    public function openCaptureModal()
+    public function capture()
     {
-        // Initialize the webcam
-        $this->initializeWebcam();
+        $this->photo = Image::canvas(400, 400)->encode('data-url');
 
-        $this->captureModal = true;
+        $this->emit('photoCaptured');
     }
 
-    public function openUploadModal()
+    public function upload()
     {
-        $this->uploadModal = true;
-    }
-
-    public function closeModal()
-    {
-        $this->captureModal = false;
-        $this->uploadModal = false;
-        $this->cropModal = false;
-        $this->photo = null;
-        $this->croppedPhoto = null;
-    }
-
-    public function capturePhoto()
-    {
-        // Get the photo from the webcam
-        $this->photo = $this->getWebcamPhoto();
-
-        // Close the capture modal and open the crop modal
-        $this->captureModal = false;
-        $this->cropModal = true;
-    }
-
-    public function uploadPhoto()
-    {
-        // Validate the uploaded photo
         $this->validate([
             'photo' => 'image|max:1024'
         ]);
 
-        // Open the crop modal
-        $this->uploadModal = false;
-        $this->cropModal = true;
+        $this->photo = Image::make($this->photo)->encode('data-url');
+
+        $this->emit('photoUploaded');
     }
 
-    public function submitPhoto()
+    public function crop()
     {
-        // Crop the photo
-        $this->croppedPhoto = $this->cropPhoto();
+        $cropped = Image::make($this->photo)
+            ->crop(
+                request()->input('cropper.height'),
+                request()->input('cropper.width'),
+                request()->input('cropper.x'),
+                request()->input('cropper.y')
+            )
+            ->encode('data-url');
 
-        // Perform any additional actions, such as saving the photo to a database or storage service
-        // ...
+        $this->photo = $cropped;
 
-        // Close the crop modal and reset the form
-        $this->cropModal = false;
-        $this->photo = null;
-        $this->croppedPhoto = null;
+        $this->emit('photoCropped');
     }
 
-    private function initializeWebcam()
+    public function closeModal()
     {
-        // Set up the webcam using webcam.js
-        $this->emit('initializeWebcam', '
-            Webcam.set({
-                width: 320,
-                height: 240,
-                image_format: "jpeg",
-                jpeg_quality: 90
-            });
-            Webcam.attach("#webcam");
-        ');
+        $this->reset();
+
+        $this->emit('modalClosed');
     }
-
-    private function getWebcamPhoto()
-    {
-        // Capture the photo from the webcam using webcam.js
-        $data = 'data:image/jpeg;base64,' . Webcam.snap();
-
-        // Save the photo to a temporary file
-        $file = $this->saveBase64Image($data);
-
-        // Return the Intervention Image instance
-        return Image::make($file);
-    }
-
-    private function saveBase64Image($data)
-    {
-        // Generate a random filename
-        $filename = str_random(16) . '.jpg';
-
-        // Save the data to a temporary file
-        \File::put(storage_path('app/public/' . $filename), base64_decode(explode(',', $data)[1]));
-
-        return storage_path('app/public/' . $filename);
-    }
-
-    private function cropPhoto()
-    {
-        // Initialize the cropper
-        $this->emit('initializeCropper', '
-            var image = document.getElementById("photo");
-            var cropper = new Cropper(image, {
-                aspectRatio: 1,
-                crop: function(e) {
-                    $("input[name=x]").val(e.detail.x);
-                    $("input[name=y]").val(e.detail.y);
-                    $("input[name=height]").val(e.detail.height);
-                    $("input[name=width]").val(e.detail.width);
-                }
-            });
-        ');
-
-        // Get the crop data from the form
-        $data = [            'x' => request()->input('x'),            'y' => request()->input('y'),            'height' => request()->input('height'),            'width' => request()->input('width'),        ];
-
-        // Crop the photo using Intervention Image
-        return Image::make($this->photo)->crop(
-            intval($data['height']),
-            intval($data['width']),
-            intval($data['x']),
-            intval($data['y'])
-        );
-    }
-
-
-
     public function render()
     {
         return view('livewire.photo-section', [
--- resources/views/livewire/photo-section.blade.php
+++ resources/views/livewire/photo-section.blade.php
@@ -1,53 +1,88 @@
 <div>
-    <!-- Button to trigger the photo capture or upload -->
-    <button wire:click="openCaptureModal">Capture Photo</button>
-    <button wire:click="openUploadModal">Upload Photo</button>
+<div>
+  @if ($photo)
+    <img src="{{ $photo }}" alt="Captured photo">
+  @else
+    <button wire:click="capture" class="btn btn-primary">Capture Photo</button>
+    <button wire:click="upload" class="btn btn-primary">Upload Photo</button>
+  @endif
+</div>
 
-    <!-- Modal to capture a photo from the webcam -->
-    <div wire:ignore wire:model="captureModal" class="modal">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h2>Capture Photo</h2>
-                <button wire:click="closeModal">&times;</button>
-            </div>
-            <div class="modal-body">
-                <!-- Webcam element -->
-                <div id="webcam"></div>
-                <!-- Button to take the photo -->
-                <button wire:click="capturePhoto">Take Photo</button>
-            </div>
-        </div>
+<!-- Modal for cropping -->
+<div wire:ignore class="modal" id="cropModal">
+  <div class="modal-content">
+    <div class="modal-header">
+      <h5 class="modal-title">Crop Photo</h5>
+      <button wire:click="closeModal" type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
     </div>
+    <div class="modal-body">
+      <div class="cropper-container">
+        <img wire:model="photo" id="cropper">
+      </div>
+    </div>
+    <div class="modal-footer">
+      <button wire:click="crop" type="button" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</div>
+@push('scripts')
+  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.0/dist/alpine.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/cropperjs@1.6.4/dist/cropper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/webcamjs@1.0.0/webcam.min.js"></script>
+  <script>
+    // Initialize cropper
+    let cropper;
+    let image = document.getElementById('cropper');
 
-    <!-- Modal to upload a photo from the computer -->
-    <div wire:ignore wire:model="uploadModal" class="modal">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h2>Upload Photo</h2>
-                <button wire:click="closeModal">&times;</button>
-            </div>
-            <div class="modal-body">
-                <!-- File input element -->
-                <input type="file" wire:model="photo" />
-                <!-- Button to select the file -->
-                <button wire:click="uploadPhoto">Select Photo</button>
-            </div>
-        </div>
-    </div>
+    function initCropper() {
+      cropper = new Cropper(image, {
+        aspectRatio: 1,
+        viewMode: 2,
+        dragMode: 'crop',
+        background: false,
+      });
+    }
 
-    <!-- Modal to crop the photo -->
-    <div wire:ignore wire:model="cropModal" class="modal">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h2>Crop Photo</h2>
-                <button wire:click="closeModal">&times;</button>
-            </div>
-            <div class="modal-body">
-                <!-- Image element to display the photo -->
-                <img id="photo" />
-                <!-- Button to submit the cropped photo -->
-                <button wire:click="submitPhoto">Submit</button>
-            </div>
-        </div>
-    </div>
+    // Close modal and reset cropper
+    function closeModal() {
+      $('#cropModal').modal('hide');
+      cropper.destroy();
+      cropper = null;
+    }
+
+    // Capture photo using webcam
+    function capture() {
+      Webcam.set({
+        width: 400,
+        height: 400,
+        image_format: 'jpeg',
+        jpeg_quality: 90
+      });
+      Webcam.attach('#cropper');
+    }
+
+    // Set photo from file input
+    function upload() {
+      $('#photoInput').click();
+    }
+
+    // Set photo from file input
+    $('#photoInput').change(function () {
+      const file = this.files[0];
+      const reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        image.src = reader.result;
+        initCropper();
+        $('#cropModal').modal('show');
+      });
+
+      reader.readAsDataURL(file);
+    });
+  </script>
+@endpush
+
+<!-- Hidden file input for uploading photos -->
+<input type="file" wire:ignore id="photoInput" accept="image/*">
+
 </div>
