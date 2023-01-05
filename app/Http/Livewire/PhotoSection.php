f1d8a9cf638adf5d2d14192e9c5d3b7ed3b421ef
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-05 03:27:24 -0600

    first commit

--- app/Http/Livewire/PhotoSection.php
+++ app/Http/Livewire/PhotoSection.php
@@ -11,87 +11,79 @@ class PhotoSection extends Component
     use WithFileUploads;
 
  
-    public $captureModalOpen = false;
-    public $uploadModalOpen = false;
-    public $cropModalOpen = false;
+ public $captureModalOpen = false;
+ public $uploadModalOpen = false;
+ public $cropModalOpen = false;
 
-    public $capturedImage;
-    public $uploadedPhoto;
-    public $croppedImage;
+ public $capturedImage;
+ public $uploadedPhoto;
+ public $croppedImage;
 
-    public function showCaptureModal()
-    {
-        $this->captureModalOpen = true;
-    }
+ public function showCaptureModal()
+ {
+     $this->captureModalOpen = true;
+ }
 
-    public function hideCaptureModal()
-    {
-        $this->reset();
-    }
+ public function hideCaptureModal()
+ {
+     $this->reset();
+ }
 
-    public function showUploadModal()
-    {
-        $this->uploadModalOpen = true;
-    }
+ public function showUploadModal()
+ {
+     $this->uploadModalOpen = true;
+ }
 
-    public function hideUploadModal()
-    {
-        $this->reset();
-    }
+ public function hideUploadModal()
+ {
+     $this->reset();
+ }
 
-    public function showCropModal()
-    {
-        $this->cropModalOpen = true;
-    }
+ public function showCropModal()
+ {
+     $this->cropModalOpen = true;
+ }
 
-    public function hideCropModal()
-    {
-        $this->reset();
-    }
+ public function hideCropModal()
+ {
+     $this->reset();
+ }
 
-    public function takePhoto()
-    {
-        Webcam.snap(function(data_uri) {
-            this.capturedImage = data_uri;
-            this.showCropModal();
-        }.bind(this));
-    }
 
-    public function uploadPhoto()
-    {
-        $this->validate([
-            'uploadedPhoto' => 'required|image',
-        ]);
+ public function uploadPhoto()
+ {
+     $this->validate([
+         'uploadedPhoto' => 'required|image',
+     ]);
 
-        $this->croppedImage = $this->uploadedPhoto->getRealPath();
-        $this->showCropModal();
-    }
+     $this->croppedImage = $this->uploadedPhoto->getRealPath();
+     $this->showCropModal();
+ }
 
-    public function cropAndSubmit()
-    {
-        $image = (new \Intervention\Image\ImageManager)->make($this->croppedImage);
+ public function cropAndSubmit()
+ {
+     $image = (new \Intervention\Image\ImageManager)->make($this->croppedImage);
 
-        // You can perform cropping and other image manipulation here using the Intervention Image library.
-        // Then, save the image and clear the form.
-        $image->save(storage_path('app/public/cropped_image.jpg'));
+     // You can perform cropping and other image manipulation here using the Intervention Image library.
+     // Then, save the image and clear the form.
+     $image->save(storage_path('app/public/cropped_image.jpg'));
 
-        // Reset form and close modal
-        $this->reset();
-    }
+     // Reset form and close modal
+     $this->reset();
+ }
 
-    public function reset()
-    {
-        $this->captureModalOpen = false;
-        $this->uploadModalOpen = false;
-        $this->cropModalOpen = false;
+ public function reset()
+ {
+     $this->captureModalOpen = false;
+     $this->uploadModalOpen = false;
+     $this->cropModalOpen = false;
 
-        $this->capturedImage = null;
-        $this->uploadedPhoto = null;
-        $this->croppedImage = null;
+     $this->capturedImage = null;
+     $this->uploadedPhoto = null;
+     $this->croppedImage = null;
 
-        $this->resetValidation();
-    }
-
+     $this->resetValidation();
+ }
     public function render()
     {
         return view('livewire.photo-section')->layout('layouts.photo');
--- resources/views/livewire/photo-section.blade.php
+++ resources/views/livewire/photo-section.blade.php
@@ -1,93 +1,55 @@
 <div>
-    <button wire:click="showCaptureModal" class="btn btn-primary">Capture Photo</button>
-    <button wire:click="showUploadModal" class="btn btn-secondary">Upload Photo</button>
-</div>
+  <button wire:click="showCaptureModal" class="capture-button">Capture</button>
+  <button wire:click="showUploadModal" class="upload-button">Upload</button>
 
-<!-- Capture Modal -->
-<div wire:ignore.self wire:model="captureModalOpen" class="modal fade">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Capture Photo</h5>
-                <button wire:click="hideCaptureModal" type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="row">
-                    <div class="col-12">
-                        <div id="capture-container" class="text-center" style="height: 400px;">
-                            <webcam wire:model="capturedImage" wire:photo.taken="photoTaken" wire:photo.saved="photoSaved" wire:photo.error="photoError" class="w-100 h-100"></webcam>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button wire:click="hideCaptureModal" type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                <button wire:click="takePhoto" class="btn btn-primary">Take Photo</button>
-            </div>
+  @if ($showCaptureModal)
+    <div class="modal">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h2>Capture Photo</h2>
+          <button wire:click="hideModal" class="close-button">&times;</button>
         </div>
-    </div>
-</div>
-
-<!-- Upload Modal -->
-<div wire:ignore.self wire:model="uploadModalOpen" class="modal fade">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Upload Photo</h5>
-                <button wire:click="hideUploadModal" type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+        <div class="modal-body">
+          <div class="capture-container">
+            <canvas wire:model="capturedImage" width="640" height="480"></canvas>
+            <div class="capture-controls">
+              <video wire:model="video" width="640" height="480" autoplay></video>
+              <button wire:click="captureImage" class="capture-button">Capture</button>
             </div>
-            <div class="modal-body">
-                <div class="row">
-                    <div class="col-12">
-                        <form wire:submit.prevent="uploadPhoto">
-                            <div class="form-group">
-                                <label for="photo">Select Photo</label>
-                                <input type="file" wire:model="uploadedPhoto" class="form-control" id="photo" accept="image/*">
-                                @error('uploadedPhoto') <span class="text-danger">{{ $message }}</span> @enderror
-                            </div>
-                            <button type="submit" class="btn btn-primary">Upload</button>
-
-                        </form>
-                    </div>
-                </div>
+          </div>
+          <div class="cropper-container" wire:ignore>
+            <img wire:model="croppedImage" alt="Cropped image">
+            <div class="cropper-controls">
+              <button wire:click="submitImage" class="submit-button">Submit</button>
+              <button wire:click="cancelCrop" class="cancel-button">Cancel</button>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  @endif
 
-<!-- Crop Modal -->
-<div wire:ignore.self wire:model="cropModalOpen" class="modal fade">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Crop Photo</h5>
-                <button wire:click="hideCropModal" type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <div class="row">
-                    <div class="col-12">
-                        <div id="crop-container" class="text-center" style="height: 400px;">
-                            <img wire:model="croppedImage" id="cropped-image" class="w-100 h-100" src="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button wire:click="hideCropModal" type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                <button wire:click="cropAndSubmit" class="btn btn-primary">Submit</button>
-            </div>
+  @if ($showUploadModal)
+    <div class="modal">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h2>Upload Photo</h2>
+          <button wire:click="hideModal" class="close-button">&times;</button>
         </div>
+        <div class="modal-body">
+          <form wire:submit.prevent="uploadImage" enctype="multipart/form-data">
+            <input type="file" name="image" accept="image/*" wire:model="uploadedImage">
+            <button type="submit" class="upload-button">Upload</button>
+          </form>
+          <div class="cropper-container" wire:ignore>
+            <img wire:model="croppedImage" alt="Cropped image">
+            <div class="cropper-controls">
+              <button wire:click="submitImage" class="submit-button">Submit</button>
+              <button wire:click="cancelCrop" class="cancel-button">Cancel</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  @endif
 </div>
-
-@push('scripts')
-<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.3.5/dist/alpine.min.js" defer></script>
-<script src="https://cdn.jsdelivr.net/npm/cropperjs@1.5.7/dist/cropper.min.js" defer></script>
-<script src="https://cdn.jsdelivr.net/npm/webcamjs@1.0.25/webcam.min.js" defer></script>
-@endpush
