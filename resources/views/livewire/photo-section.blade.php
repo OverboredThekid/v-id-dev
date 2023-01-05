5fcd195ac6bdf091954419f8e338127dd8790b5a
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-05 11:21:19 -0600

    first commit

--- resources/views/livewire/photo-section.blade.php
+++ resources/views/livewire/photo-section.blade.php
@@ -1,42 +1,43 @@
 <div>
+    <canvas id="captureCanvas"></canvas>
     <div wire:ignore>
         <button wire:click="showCaptureForm">Capture</button>
         <button wire:click="showUploadForm">Upload</button>
     </div>
     <div wire:ignore wire:loading.class="hidden">
         @if ($showCaptureForm)
-            <div wire:ignore>
-                <canvas id="captureCanvas"></canvas>
-                <button x-data x-on:click="capturePhoto()">Capture</button>
-            </div>
+        <div wire:ignore>
+
+            <button x-data x-on:click="capturePhoto()">Capture</button>
+        </div>
         @endif
         @if ($showUploadForm)
-            <div wire:ignore>
-                <input type="file" wire:model="photo" accept="image/*">
-            </div>
+        <div wire:ignore>
+            <input type="file" wire:model="photo" accept="image/*">
+        </div>
         @endif
         @if ($photo)
+        <div>
+            <img id="photoPreview" src="{{ $photo->temporaryUrl() }}">
+        </div>
+        <div wire:ignore>
+            <button wire:click="openCropModal">Crop</button>
+        </div>
+        <div wire:ignore wire:model="showCropModal">
             <div>
-                <img id="photoPreview" src="{{ $photo->temporaryUrl() }}">
+                <img id="croppedPhotoPreview" src="{{ $croppedPhotoUrl }}">
             </div>
-            <div wire:ignore>
-                <button wire:click="openCropModal">Crop</button>
+            <div>
+                <button wire:click="submitPhoto">Submit</button>
+                <button wire:click="closeCropModal">Cancel</button>
             </div>
-            <div wire:ignore wire:model="showCropModal">
-                <div>
-                    <img id="croppedPhotoPreview" src="{{ $croppedPhotoUrl }}">
-                </div>
-                <div>
-                    <button wire:click="submitPhoto">Submit</button>
-                    <button wire:click="closeCropModal">Cancel</button>
-                </div>
-            </div>
+        </div>
         @endif
     </div>
 </div>
 @push('scripts')
 <script>
-    setTimeout(function () {
+    setTimeout(function() {
         Webcam.set({
             width: 320,
             height: 240,
@@ -47,7 +48,7 @@
     }, 1000);
 
     function capturePhoto() {
-        Webcam.snap(function (data_uri) {
+        Webcam.snap(function(data_uri) {
             window.livewire.emit('photoCaptured', data_uri);
         });
     }
