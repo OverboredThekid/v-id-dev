fc40b59098da0abf77344b067fee91a75c8c34e9
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-04 23:42:59 -0600

    first commit

--- app/Http/Livewire/PhotoSection.php
+++ app/Http/Livewire/PhotoSection.php
@@ -4,83 +4,61 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use Livewire\WithFileUploads;
-use Intervention\Image\Facades\Image;
 
 class PhotoSection extends Component
 {
     use WithFileUploads;
 
-    // state variables
-    public $photo;
-    public $croppedPhoto;
-    public $webcamOn;
-    public $fileOn;
+    public $image;
+    public $croppedImage;
 
-    // initialize state variables
     public function mount()
     {
-        $this->webcamOn = false;
-        $this->fileOn = false;
-    }
-
-    // toggle webcam on and off
-    public function toggleWebcam()
-    {
-        $this->webcamOn = !$this->webcamOn;
-
-        if ($this->webcamOn) {
-            Webcam.attach('#my_camera');
-        } else {
-            Webcam.reset();
-        }
-    }
-
-    // capture photo from webcam
-    public function capture()
-    {
-        Webcam.snap(function(data_uri) {
-            this.photo = data_uri;
-            this.webcamOn = false;
-            this.fileOn = true;
-        });
-    }
-
-    // toggle file input on and off
-    public function toggleFile()
-    {
-        this.fileOn = !this.fileOn;
-    }
-
-    // select photo from file input
-    public function updatedPhoto($photo)
-    {
-        this.photo = $photo;
-        this.fileOn = true;
-    }
-
-    // crop photo
-    public function crop()
-    {
-        // create cropper
-        $cropper = new Cropper(this.refs.photo, [
-            'aspectRatio' => 1,
-            'crop' => function($event) {
-                $this->croppedPhoto = $event['detail']['canvas']->toDataURL();
-            }.bind(this)
-        ]);
-    
-        // crop photo
-        $cropper->crop();
-    }
-
-    // submit photo
-    public function submit()
-    {
-        this.$emit('photoSelected', this.croppedPhoto);
+        // Initialize WebcamJS
+        $this->emit('initWebcam');
     }
 
     public function render()
     {
-        return view('livewire.photo-section')->layout('layouts.photo');
+        return view('livewire.photo-section');
+    }
+
+    public function capture()
+    {
+        // Capture image from webcam and emit it to the frontend
+        $this->emit('capture');
+    }
+
+    public function openFilePicker()
+    {
+        // Open file picker to select image from computer
+        $this->emit('openFilePicker');
+    }
+
+    public function upload()
+    {
+        // Validate uploaded file
+        $this->validate([
+            'image' => 'required|image|max:1024'
+        ]);
+
+        // Emit uploaded image to the frontend
+        $this->image = $this->image->temporaryUrl();
+        $this->emit('upload');
+    }
+
+    public function crop($image)
+    {
+        // Set cropped image and emit it to the frontend
+        $this->croppedImage = $image;
+        $this->emit('crop', $image);
+    }
+
+    public function save()
+    {
+        // Save the cropped image to the server
+        // You can implement your own code here to save the image to the server
+        // You may also want to delete the original image or the captured image if you don't need them anymore
+        $this->emit('save');
     }
 }
--- resources/views/livewire/photo-section.blade.php
+++ resources/views/livewire/photo-section.blade.php
@@ -1,66 +1,136 @@
-<!-- blade view HTML -->
 <div>
-    <!-- webcam toggle button -->
-    <button
-        x-on:click="toggleWebcam"
-        x-bind:class="{ 'hidden': fileOn }"
-    >
-        Toggle Webcam
-    </button>
-
-    <!-- file input toggle button -->
-    <button
-        x-on:click="toggleFile"
-        x-bind:class="{ 'hidden': webcamOn }"
-    >
-        Choose Photo
-    </button>
-
-    <!-- webcam -->
-    <div
-        x-bind:class="{ 'hidden': !webcamOn }"
-    >
-        <div id="my_camera"></div>
-        <button x-on:click="capture">Capture</button>
+    <div id="app" wire:ignore>
+        <div class="container mt-5">
+            <div class="row justify-content-center">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="text-center">Photo Capture and Crop</h3>
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div id="my_camera"></div>
+                                    <br>
+                                    <button wire:click="capture()" class="btn btn-primary btn-block">Capture Photo</button>
+                                </div>
+                                <div class="col-md-6">
+                                    <div id="results">Your captured photo will appear here...</div>
+                                </div>
+                            </div>
+                            <br>
+                            <div class="row">
+                                <div class="col-md-12 text-center">
+                                    <input type="file" id="upload-photo" accept="image/*" wire:change="upload" class="d-none">
+                                    <button wire:click="openFilePicker" class="btn btn-secondary btn-block">
+                                        Choose Photo from Computer
+                                    </button>
+                                </div>
+                            </div>
+                            <br>
+                            @if ($image)
+                                <div class="row">
+                                    <div class="col-md-12 text-center">
+                                        <div id="uploaded-photo" class="image-container">
+                                            <img wire:loading wire:target="upload" src="{{ $image }}" alt="your image" class="img-fluid" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <div class="col-md-12 text-center">
+                                        <button wire:click="crop" class="btn btn-primary btn-block">Crop Photo</button>
+                                    </div>
+                                </div>
+                            @endif
+                            @if ($croppedImage)
+                                <div class="row mt-5">
+                                    <div class="col-md-12 text-center">
+                                        <div id="cropped-photo" class="image-container">
+                                            <img src="{{ $croppedImage }}" alt="your cropped image" class="img-fluid" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <div class="col-md-12 text-center">
+                                        <button wire:click="save" class="btn btn-success btn-block">Save Photo</button>
+                                    </div>
+                                </div>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-
-    <!-- file input -->
-    <div
-        x-bind:class="{ 'hidden': !fileOn }"
-    >
-        <input
-            type="file"
-            wire:model="photo"
-            x-on:change="updatedPhoto"
-        />
-    </div>
-
-    <!-- photo preview -->
-    <img
-        x-bind:src="photo"
-        x-bind:class="{ 'hidden': !fileOn && !webcamOn }"
-        x-ref="photo"
-    />
-
-    <!-- crop button -->
-    <button
-        x-on:click="crop"
-        x-bind:class="{ 'hidden': !fileOn && !webcamOn }"
-    >
-        Crop
-    </button>
-
-    <!-- cropped photo preview -->
-    <img
-        x-bind:src="croppedPhoto"
-        x-bind:class="{ 'hidden': !croppedPhoto }"
-    />
-
-    <!-- submit button -->
-    <button
-        x-on:click="submit"
-        x-bind:class="{ 'hidden': !croppedPhoto }"
-    >
-        Submit
-    </button>
 </div>
+<script>
+    let cropper;
+
+    function showCroppedImage(img) {
+        var image = document.createElement('img');
+        image.src = img;
+        var container = document.getElementById('cropped-photo');
+        container.innerHTML = '';
+        container.appendChild(image);
+    }
+
+    function capture() {
+        webcam.capture(function (data_uri) {
+            document.getElementById('results').innerHTML = '<img src="' + data_uri + '"/>';
+            let image = document.querySelector('#results img');
+            image.setAttribute('id', 'captured-photo');
+            cropper = new Cropper(image, {
+                aspectRatio: 1,
+                crop: function(event) {
+                    console.log(event.detail.x);
+                    console.log(event.detail.y);
+                    console.log(event.detail.width);
+                    console.log(event.detail.height);
+                    console.log(event.detail.rotate);
+                    console.log(event.detail.scaleX);
+                    console.log(event.detail.scaleY);
+                }
+            });
+        });
+    }
+
+    function crop() {
+        let canvas = cropper.getCroppedCanvas();
+        let image = canvas.toDataURL('image/jpeg', 0.8);
+        showCroppedImage(image);
+        window.livewire.emit('crop', image);
+    }
+
+    function openFilePicker() {
+        document.getElementById('upload-photo').click();
+    }
+
+    window.livewire.on('upload', () => {
+        let fileInput = document.getElementById('upload-photo');
+        let file = fileInput.files[0];
+        let reader = new FileReader();
+        reader.onload = function () {
+            let image = new Image();
+            image.src = reader.result;
+            image.setAttribute('id', 'uploaded-photo');
+            let container = document.getElementById('uploaded-photo');
+            container.innerHTML = '';
+            container.appendChild(image);
+            cropper = new Cropper(image, {
+                aspectRatio: 1,
+                crop: function(event) {
+                    console.log(event.detail.x);
+                    console.log(event.detail.y);
+                    console.log(event.detail.width);
+                    console.log(event.detail.height);
+                    console.log(event.detail.rotate);
+                    console.log(event.detail.scaleX);
+                    console.log(event.detail.scaleY);
+                }
+            });
+        };
+        reader.readAsDataURL(file);
+    });
+</script>
