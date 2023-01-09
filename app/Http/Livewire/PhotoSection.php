0d5f6912e391686f5fd51c28f2eec9c41fa4ee6c
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-09 15:51:53 -0600

    first commit

--- app/Http/Livewire/PhotoSection.php
+++ app/Http/Livewire/PhotoSection.php
@@ -12,7 +12,23 @@ class PhotoSection extends Component
     use WithFileUploads;
 
 
+    public $image;
 
+    public function mount()
+    {
+        $this->image = null;
+    }
+
+    public function sendBase64Image($imageData)
+    {
+        // Process the image data as needed, for example,
+        // save it to the server or store it in the database
+        $this->image = $imageData;
+
+        // Emit an event with the image data
+        $this->emit('imageProcessed', $this->image);
+    }
+    
     public function upload()
     {
         return response()->json(['success'=>'success']);
