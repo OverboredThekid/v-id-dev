936cd2aecd013006cf48222cf05d4e9f3c8235e3
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-09 17:39:26 -0600

    first commit

--- app/Http/Livewire/StaffWizard.php
+++ app/Http/Livewire/StaffWizard.php
@@ -56,7 +56,7 @@ class StaffWizard extends Component
 
         // Convert the base64 data to a TemporaryUploadedFile object
         $file = Image::make($this->imageData)->encode('jpg');
-        $temp_path = public_path('tmp/' . time() . '.jpg');
+        $temp_path = storage_path('tmp/' . time() . '.jpg');
         $file->save($temp_path);
         $file = new TemporaryUploadedFile($temp_path, 'image.jpg', 'image/jpeg', null, true);
 
