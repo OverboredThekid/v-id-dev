72a0ab9a80a7c7b78e0d8934fe4626741ecb24ee
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-09 15:35:25 -0600

    first commit

--- app/Http/Livewire/PhotoSection.php
+++ app/Http/Livewire/PhotoSection.php
@@ -11,30 +11,13 @@ class PhotoSection extends Component
 {
     use WithFileUploads;
 
-    public $capturedImage;
-    public $uploadedImage;
-    public $croppedImage;
 
-    public function capture()
-    {
-        if (isset($_POST['image'])) {
-            $this->capturedImage = Image::make($_POST['image'])->encode('jpg');
-            $this->emit('showModal');
-        }
-    }
 
     public function upload()
     {
-        $this->uploadedImage = Image::make($this->image)->encode('jpg');
-        $this->emit('showModal');
+        return response()->json(['success'=>'success']);
     }
 
-    public function crop()
-    {
-        $this->croppedImage = Image::make($_POST['image'])
-            ->crop((int) $_POST['x'], (int) $_POST['y'], (int) $_POST['width'], (int) $_POST['height'])
-            ->encode('jpg');
-    }
     public function render()
     {
         return view('livewire.photo-section')->layout('layouts.photo');
