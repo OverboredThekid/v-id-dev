12d02138a2e95703c8cde5166b37556bd360cee2
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-09 19:04:51 -0600

    first commit

--- resources/views/livewire/photo-section.blade.php
+++ resources/views/livewire/photo-section.blade.php
@@ -24,103 +24,139 @@
             max-width: 1000px !important;
         }
     </style>
-    <div class="container">
-        <h1>Image Upload</h1>
-        <input type="file" name="image" class="image">
-    </div>
-
-    <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="modalLabel">Crop Image To Size</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="img-container">
-                        <div class="row">
-                            <div class="col-md-8">
-                                <img id="image" src="https://avatars0.githubusercontent.com/u/3456749">
-                            </div>
-                            <div class="col-md-4">
-                                <div class="preview"></div>
+    <div>
+        <div class="container">
+            <h1>Image Upload</h1>
+            <input type="file" name="image" class="image">
+            <br>
+            <br>
+            <video id="webcam" autoplay></video>
+            <br>
+            <br>
+            <button id="take-photo" class="btn btn-primary">Take Photo</button>
+        </div>
+        <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modalLabel">Crop Image To Size</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="img-container">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <img id="image" src="https://avatars0.githubusercontent.com/u/3456749">
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="preview"></div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    <button type="button" class="btn btn-primary" id="crop">Crop</button>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="button" class="btn btn-primary" id="crop">Crop</button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    <script>
+        // Get the video element
+        var video = document.getElementById('webcam');
 
-</div>
-</div>
-<script>
-    var $modal = $('#modal');
-    var image = document.getElementById('image');
-    var cropper;
-
-    $("body").on("change", ".image", function(e) {
-        var files = e.target.files;
-        var done = function(url) {
-            image.src = url;
-            $modal.modal('show');
-        };
-        var reader;
-        var file;
-        var url;
-
-        if (files && files.length > 0) {
-            file = files[0];
-
-            if (URL) {
-                done(URL.createObjectURL(file));
-            } else if (FileReader) {
-                reader = new FileReader();
-                reader.onload = function(e) {
-                    done(reader.result);
-                };
-                reader.readAsDataURL(file);
-            }
-        }
-    });
-
-    $modal.on('shown.bs.modal', function() {
-        cropper = new Cropper(image, {
-            aspectRatio: 1,
-            viewMode: 3,
-            preview: '.preview'
-        });
-    }).on('hidden.bs.modal', function() {
-        cropper.destroy();
-        cropper = null;
-    });
-
-    $("#crop").click(function() {
-        canvas = cropper.getCroppedCanvas({
-            width: 360,
-            height: 360,
+        // Get access to the user's webcam
+        navigator.mediaDevices.getUserMedia({
+            video: true
+        }).then(function(stream) {
+            video.srcObject = stream;
         });
 
-        canvas.toBlob(function(blob) {
-            url = URL.createObjectURL(blob);
-            var reader = new FileReader();
-            reader.readAsDataURL(blob);
-            reader.onloadend = function() {
-                var base64data = reader.result;
+        // Get the "Take Photo" button
+        var takePhotoButton = document.getElementById('take-photo');
 
-                // Emit an event with the image data
-                window.livewire.emit('sendBase64Image', base64data);
-
-                // Close the modal
-                $modal.modal('hide');
-            }
+        // Add an event listener to the button
+        takePhotoButton.addEventListener('click', function() {
+                    // Get the canvas element
+                    var canvas = document.createElement('canvas');
+                    // Set the canvas to the same dimensions as the video
+                    canvas.width = video.videoWidth;
+                    canvas.height = video.videoHeight;
+                    // Get the canvas context
+                    var context = canvas.getContext('2d');
+                    // Draw the current frame of the video onto the canvas
+                    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+                    // Get the image data from the canvas
+                    var imageData = canvas.toDataURL('image/png');
+                    // Set the src of the image element to the image data
+                    image.src = imageData;
+                    // Show the modal
+                    $modal.modal('show');
         });
-    });
-</script>
+
+                    var $modal = $('#modal');
+                    var image = document.getElementById('image');
+                    var cropper;
+
+                    $("body").on("change", ".image", function(e) {
+                        var files = e.target.files;
+                        var done = function(url) {
+                            image.src = url;
+                            $modal.modal('show');
+                        };
+                        var reader;
+                        var file;
+                        var url;
+
+                        if (files && files.length > 0) {
+                            file = files[0];
+
+                            if (URL) {
+                                done(URL.createObjectURL(file));
+                            } else if (FileReader) {
+                                reader = new FileReader();
+                                reader.onload = function(e) {
+                                    done(reader.result);
+                                };
+                                reader.readAsDataURL(file);
+                            }
+                        }
+                    });
+
+                    $modal.on('shown.bs.modal', function() {
+                        cropper = new Cropper(image, {
+                            aspectRatio: 1,
+                            viewMode: 3,
+                            preview: '.preview'
+                        });
+                    }).on('hidden.bs.modal', function() {
+                        cropper.destroy();
+                        cropper = null;
+                    });
+
+                    $("#crop").click(function() {
+                        canvas = cropper.getCroppedCanvas({
+                            width: 360,
+                            height: 360,
+                        });
+
+                        canvas.toBlob(function(blob) {
+                            url = URL.createObjectURL(blob);
+                            var reader = new FileReader();
+                            reader.readAsDataURL(blob);
+                            reader.onloadend = function() {
+                                var base64data = reader.result;
+
+                                // Emit an event with the image data
+                                window.livewire.emit('sendBase64Image', base64data);
+
+                                // Close the modal
+                                $modal.modal('hide');
+                            }
+                        });
+                    });
+    </script>
 </div>
