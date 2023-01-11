d1cc074b6bed2bbef1bd75c8e79c780e0326cd52
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-10 22:07:08 -0600

    first commit

--- resources/views/livewire/photo-section.blade.php
+++ resources/views/livewire/photo-section.blade.php
@@ -67,7 +67,7 @@
                     </div>
                 </div>
             </div>
-            <div class="modal-footer fixed-bottom">
+            <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
                         <button type="button" class="btn btn-primary" id="crop">Crop</button>
                     </div>
