ba79b0d4c37ca18e9978a9e6c6a0b60db32c96c2
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-11 14:01:54 -0600

    first commit

--- app/Settings/BadgeSettings.php
+++ app/Settings/BadgeSettings.php
@@ -3,13 +3,12 @@
 namespace app\Settings;
 
 use Spatie\LaravelSettings\Settings;
-use Spatie\LaravelSettings\SettingsCasts\DateTimeZoneCast as DateTime;
 
 class BadgeSettings extends Settings
 {
     public string $qr_link;
     public array $svg_front;
-    public DateTime $exp_date;
+    public string $exp_date;
     public array $svg_back;
     public bool $is_redirect;
 
