64379f413a6fcc6fa2ab8cb6cbfa2aa875e5fa4d
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-11 13:53:44 -0600

    first commit

--- app/Settings/BadgeSettings.php
+++ app/Settings/BadgeSettings.php
@@ -7,9 +7,9 @@ use Spatie\LaravelSettings\Settings;
 class BadgeSettings extends Settings
 {
     public string $qr_link;
-    public string $svg_front;
+    public array $svg_front;
     public string $exp_date;
-    public string $svg_back;
+    public array $svg_back;
     public bool $is_redirect;
 
     public static function group(): string
