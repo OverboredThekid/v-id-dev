c90840f2cd208b52e67706aa300d3d834ca11dab
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-11 01:06:17 -0600

    first commit

--- app/Http/Middleware/IsActive.php
+++ app/Http/Middleware/IsActive.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use App\Models\staff;
-use App\Settings\BadgeSettings;
+use app\Settings\BadgeSettings;
 
 class IsActive
 {
@@ -17,28 +17,47 @@ class IsActive
     {
         return app(BadgeSettings::class)->qr_link;
     }
-
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
     public function handle(Request $request, Closure $next)
     {
         // Retrieve the employee record using the slug passed in the route
         $staff = staff::where('id', $request->slug)->first();
-
-        // Check if the employee record was found
-        if ($staff) {
-            if ($staff->is_active && $this->isredirect() ) {
-                // If the employee is active and redirect is true, allow the request to proceed
-                return redirect($this->qrlink());
-            } else {
-                if (!$staff->is_active) {
+        if ($this->isredirect() == false) {
+            // Check if the employee record was found
+            if ($staff) {
+                // If the employee record was found, check if the employee is active
+                if ($staff->is_active) {
+                    // If the employee is active, allow the request to proceed
+                    return $next($request);
+                } else {
+                    // If the employee is not active, redirect the user to a 404 error page
                     return abort(403, 'This Staff Member Is Not Active.');
                 }
-                if (!$this->isredirect()) {
-                    return $next($request);
-                }
+            } else {
+                // If the employee record was not found, redirect the user to a 404 error page
+                return abort(403, 'This Staff Member Was Not Found');
             }
-        } else {
-            // If the employee record was not found, redirect the user to a 404 error page
-            return abort(404, 'This Staff Member Was Not Found');
+        } elseif ($this->isredirect() == true) {
+            // Check if the employee record was found
+            if ($staff) {
+                // If the employee record was found, check if the employee is active
+                if ($staff->is_active) {
+                    // If the employee is active, allow the request to proceed
+                    return redirect($this->qrlink());
+                } else {
+                    // If the employee is not active, redirect the user to a 404 error page
+                    return abort(403, 'This Staff Member Is Not Active.');
+                }
+            } else {
+                // If the employee record was not found, redirect the user to a 404 error page
+                return abort(403, 'This Staff Member Was Not Found');
+            }
         }
     }
 }
