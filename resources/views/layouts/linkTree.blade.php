72bbea9cb6f3d9521e0293608c81572abf510239
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-13 13:25:28 -0600

    first commit

--- resources/views/layouts/linkTree.blade.php
+++ resources/views/layouts/linkTree.blade.php
@@ -8,7 +8,21 @@
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.css" rel="stylesheet">
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-170187421-5"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-170187421-5');
+    </script>
+
 </head>
+
 <body>
     {{ $slot }}
 </body>
