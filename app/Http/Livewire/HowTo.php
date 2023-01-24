c9713dc73f2fe5810da26d7c4b71ac466b0ea003
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-24 17:14:47 -0600

    first commit

--- app/Http/Livewire/HowTo.php
+++ app/Http/Livewire/HowTo.php
@@ -12,7 +12,7 @@ class HowTo extends Component
     {
         $client = new Client();
     
-        $response = $client->get('https://wiki.v-id.dev/api/pages/2', [
+        $response = $client->get('https://wiki.v-id.dev/api/pages/7', [
             'headers' => [
                 'Authorization' => 'Token 2C0QC7d2Gy2tS92agBzOBNMQ92gjlhEG:RwLyat52Chg7GSRvH38rnE1658SWoJPK'
             ]
