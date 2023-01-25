b730d96ea6ab91066dd6996675e5cc0d926de8b5
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-24 18:03:52 -0600

    first commit

--- resources/views/livewire/how-to.blade.php
+++ resources/views/livewire/how-to.blade.php
@@ -1,4 +1,6 @@
 <div>
-<button wire:click="synchronizeBookstack">Sync Books</button>
+<button wire:click="synchronizeBookstack" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+  Sync
+</button>
 {!! $bookstack->html !!}
 </div>
