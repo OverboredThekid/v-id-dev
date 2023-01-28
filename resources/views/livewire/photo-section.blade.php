ddde3276c71ad1245a6338821ed9e940c3a54bb7
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-27 19:08:21 -0600

    first commit

--- resources/views/livewire/photo-section.blade.php
+++ resources/views/livewire/photo-section.blade.php
@@ -27,55 +27,68 @@
         body {
             overflow: auto;
         }
+         /* Add grayed out background to modal */
+         .modal-backdrop {
+            background-color: rgba(0,0,0,0.5);
+        }
     </style>
     <!--Using Tailwind CSS classes for responsive design-->
-<div class="flex flex-col">
-    <label class="btn btn-primary">
-        <input type="file" name="image" class="image" accept="image/*" style="display: none;">Upload A Photo
-    </label>
-    <br>
-    <div class="embed-responsive embed-responsive-16by9">
-        <video id="webcam" width="100%" height="100%" autoplay class="embed-responsive-item" style="display: none;" playsinline></video>
-    </div>
-    <div id="loading-message" style="display: none;">Loading webcam...</div>
-    <br>
-    <button id="activate-webcam" class="btn btn-primary">Take A Photo</button>
-    <div class="flex justify-between">
-        <button id="take-photo" class="btn btn-primary" style="display: none;">Capture</button>
-        <button id="turn-off-webcam" class="btn btn-primary" style="display: none;">Turn Off Webcam</button>
-    </div>
+    <div>
+    <div>
+        <label class="btn btn-primary">
+            <input type="file" name="image" class="image" accept="image/*" style="display: none;">Upload A Photo
+        </label>
+        <br>
 
-    <br>
+        <div class="embed-responsive embed-responsive-16by9">
+            <video id="webcam" width="100%" height="100%" autoplay class="embed-responsive-item" style="display: none;" playsinline></video>
+        </div>
 
-    <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document" style="margin: 0">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="modalLabel">Crop Image To Size</h5>
-                    <button type="button" class="close" style="background-color:#fff;" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <div class="img-container">
-                        <div class="flex">
-                            <div class="w-2/3">
-                                <img id="image" src="https://avatars0.githubusercontent.com/u/3456749" class="block max-w-full">
-                            </div>
-                            <div class="w-1/3">
-                                <div class="preview overflow-hidden w-40 h-40 m-2 border border-red-500"></div>
+        <div id="loading-message" style="display: none;">Loading webcam...</div>
+        <br>
+        <button id="activate-webcam" class="btn btn-primary">Take A Photo</button>
+
+        <div class="row">
+            <button id="take-photo" class="btn btn-primary" style="display: none; margin: right 15px;">Capture</button>
+            <button id="turn-off-webcam" class="btn btn-primary" style="display: none;">Turn Off Webcam</button>
+        </div>
+
+        <br>
+
+        <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document" style="margin: 0">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="modalLabel">Crop Image To Size</h5>
+                        <button type="button" class="close" style="background-color:#fff;" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+
+                    <div class="modal-body">
+                        <div class="img-container">
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <img id="image" src="https://avatars0.githubusercontent.com/u/3456749">
+                                </div>
+
+                                <div class="col-md-4">
+                                    <div class="preview"></div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-                    <button type="button" class="btn btn-primary" id="crop">Crop</button>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="button" class="btn btn-primary" id="crop">Crop</button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
 
     <script>
         // Get the video element
