7dbde34d004b92a0b82ad048e52e8f8939e3f0a5
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-27 18:52:23 -0600

    first commit

--- resources/views/livewire/photo-section.blade.php
+++ resources/views/livewire/photo-section.blade.php
@@ -32,7 +32,7 @@
         <label class="btn btn-primary"><input type="file" name="image" class="image" accept="image/*" style="display: none;">Upload A Photo</label>
         <br>
         <div class="embed-responsive embed-responsive-16by9">
-            <video id="webcam" width="100%" height="100%" autoplay class="embed-responsive-item" style="display: none;"></video>
+            <video id="webcam" width="100%" height="100%" autoplay class="embed-responsive-item" style="display: none;" playsinline></video>
         </div>
         <div id="loading-message" style="display: none;">Loading webcam...</div>
         <br>
