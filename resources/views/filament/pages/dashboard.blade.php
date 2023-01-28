6607c7871a2049ab4b5e0a6f955fd711f2c77685
Bennett haynes <bennetthaynes1@icloud.com>
2023-01-27 21:03:11 -0600

    first commit

--- resources/views/filament/pages/dashboard.blade.php
+++ resources/views/filament/pages/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-filament::page>
 <livewire:how-to />
 <!-- Pin to bottom right corner -->
-<div class="relative h-32 mt-16">
+<div class="relative h-32 mt-50">
   <div class="absolute bottom-0 right-0">Need More Help? Click <a href="https://wiki.v-id.dev" class="font-bold">HERE</a> to visit the Wiki</div>
 </div>
 </x-filament::page>
